8940baec3cbfa32f07ec3c51c32a4592fe346066
KishorRShetty <36321276+KishorRShetty@users.noreply.github.com>
2022-03-25 00:12:48 +0530

    saturation 50% to make it bright

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.post("/quoteToPng", (req, res) => {
         margin: 5,
         verticalAlign: 'center',
         textAlign:'center',
-        textColor: `hsla(${req.body.color.hue},${req.body.color.saturation * 100}%,${
+        textColor: `hsla(${req.body.color.hue},${req.body.color.saturation * 50}%,${
             req.body.color.brightness * 100
           }%,${req.body.color.alpha})`,
       });
